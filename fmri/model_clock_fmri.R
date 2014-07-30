08969d7705ea48a30ef763491924aac5e4ccbda4
Michael Hallquist <michael.hallquist@gmail.com>
2014-07-30 12:37:00 -0400

    remove timing files from repo, move fit files into root fmri_fits, develop fsl R code to run feat automatically and in parallel.

--- fmri/model_clock_fmri.R
+++ fmri/model_clock_fmri.R
@@ -2,16 +2,122 @@
 # setup model-based fMRI GLM analysis for based on fitted data
 
 library(fitclock)
+library(Rniftilib) #has nice function for just reading header
+source(file.path(getMainDir(), "Miscellaneous", "Global_Functions.R"))
 
-behavDir <- "/Volumes/bea_res/Data/Tasks/EmoClockfMRI/Basic"
-fmriDir <- "/Volumes/Serena/MMClock/MR_Raw"
-behavFiles <- list.files(path=behavDir, pattern="*tcExport.csv", full.names=TRUE, recursive=TRUE)
+fslValueModel <- function(mrfiles, fitobj, run=FALSE) {
+  require(Rniftilib)
+  require(parallel)
+  fsfTemplate <- readLines(file.path(getMainDir(), "clock_analysis", "fmri", "feat_lvl1_clock_template.fsf"))
+  
+  fsldir <- file.path(normalizePath(file.path(dirname(mrfiles[1L]), "..")), "fsl_value") #note: normalizePath will fail to evaluate properly if directory does not exist
+  cat("fsldir create: ", fsldir, "\n")
+  dir.create(fsldir, showWarnings=FALSE) #one directory up from a given clock run
+  timingdir <- file.path(fsldir, "run_timing_deltavalue")
+  
+  d_value <- fitobj$build_design_matrix(regressors=c("clock", "feedback", "ev", "rpe_neg", "rpe_pos"), 
+      event_onsets=c("clock_onset", "feedback_onset", "clock_onset", "feedback_onset", "feedback_onset"), 
+      durations=c(0, 0, "clock_duration", "feedback_duration", "feedback_duration"), 
+      baselineCoefOrder=2, writeTimingFiles=c("FSL"),
+      runVolumes=runlengths, output_directory=timingdir)
+  
+  allFeatFiles <- list()
+  
+  #FSL computes first-level models on individual runs
+  for (r in mrfiles) {
+    stopifnot(file.exists(file.path(dirname(r), "motion.par"))) #can't find motion parameters
+    
+    runnum <- sub("^.*/clock(\\d+)$", "\\1", dirname(r), perl=TRUE)
+    nvol <- nifti.image.read(r, read_data=0)$dim[4L]
+    
+    #for motion parameter regression, add temporal derivatives, then PCA and retain first 3 components (~95% of variance)
+    mot <- read.table(file.path(dirname(r), "motion.par"), col.names=c("r.x", "r.y", "r.z", "t.x", "t.y", "t.z"))
+    motderiv <- as.data.frame(lapply(mot, function(col) { c(0, diff(col)) }))
+    names(motderiv) <- paste0("d.", names(motderiv)) #add delta to names
+    motall <- cbind(mot, motderiv)
+    pc <- princomp(motall, scores=TRUE)
+    cumvar <- cumsum(pc$sdev^2/sum(pc$sdev^2))
+    cat("first three motion principal components account for: ", plyr::round_any(cumvar[3], .001), "\n")
+    mregressors <- pc$scores[,1:3] #first three components
+    
+    #add volumes to censor here... with wavelet despiking, this becomes less clear
+    motfile <- file.path(fsldir, paste0("run", runnum, "_mot_regress_3pc.txt"))
+    write.table(mregressors, file=motfile, col.names=FALSE, row.names=FALSE)
+    
+    #add deep ventricle time series as confound regressor?
+    
+    #search and replace within fsf file for appropriate sections
+    #.OUTPUTDIR. is the feat output location
+    #.NVOL. is the number of volumes in the run
+    #.FUNCTIONAL. is the fmri data to process (sans extension)
+    #.CONFOUNDS. is the confounds file for GLM
+    #.CLOCK_TIMES. is the three-column file for clock onset
+    #.FEEDBACK_TIMES. is the three-column file for feedback onset
+    #.EV_TIMES. is the three-column file for expected value
+    #.RPEPOS_TIMES. is the three-column file for positive rpes
+    #.RPENEG_TIMES. is the three-column file for negative rpes
+    
+    thisTemplate <- fsfTemplate
+    thisTemplate <- gsub(".OUTPUTDIR.", file.path(fsldir, paste0("FEAT_LVL1_run", runnum)), thisTemplate, fixed=TRUE)
+    thisTemplate <- gsub(".NVOL.", nvol, thisTemplate, fixed=TRUE)
+    thisTemplate <- gsub(".FUNCTIONAL.", gsub(".nii(.gz)*$", "", r), thisTemplate, fixed=TRUE)
+    thisTemplate <- gsub(".CONFOUNDS.", motfile, thisTemplate, fixed=TRUE)
+    thisTemplate <- gsub(".CLOCK_TIMES.", file.path(timingdir, paste0("run", runnum, "_clock_FSL3col.txt")), thisTemplate, fixed=TRUE)
+    thisTemplate <- gsub(".FEEDBACK_TIMES.", file.path(timingdir, paste0("run", runnum, "_feedback_FSL3col.txt")), thisTemplate, fixed=TRUE)
+    thisTemplate <- gsub(".EV_TIMES.", file.path(timingdir, paste0("run", runnum, "_ev_FSL3col.txt")), thisTemplate, fixed=TRUE)
+    thisTemplate <- gsub(".RPEPOS_TIMES.", file.path(timingdir, paste0("run", runnum, "_rpe_pos_FSL3col.txt")), thisTemplate, fixed=TRUE)
+    thisTemplate <- gsub(".RPENEG_TIMES.", file.path(timingdir, paste0("run", runnum, "_rpe_neg_FSL3col.txt")), thisTemplate, fixed=TRUE)
+    
+    featFile <- file.path(fsldir, paste0("FEAT_LVL1_run", runnum, ".fsf"))
+    cat(thisTemplate, file=featFile, sep="\n")
+    
+    allFeatFiles[[r]] <- featFile
+  }
+  
+  if (run == TRUE) {
+    #N.B.: for some reason, running feat using a system call hangs, whereas running it in bash is okay
+    #I think it has something to do with trying to open the report in the browser at initialization, but I'm not sure...
+    #looks like it has to do with system2 running a non-interactive shell
+    #force interactive run using bash -i
+    #for now, generate a bash script to run it.
+#    cat("#!/bin/bash",
+#        paste0(file.path(Sys.getenv("FSLDIR"), "bin", "feat "), allFeatFiles, 
+#            " > ", sapply(allFeatFiles, dirname), "/feat_stdout_", sapply(allFeatFiles, "basename"),
+#            " 2> ", sapply(allFeatFiles, dirname), "/feat_stderr_", sapply(allFeatFiles, "basename"),
+#            " &"),
+#        "wait",
+#        file="runfeat.bash", sep="\n")
+
+    #browser() 
+    #system("bash -i runfeat.bash")
+    #system2("bash", "runfeat.bash")
+  
+    cl_fork <- makeForkCluster(nnodes=8)
+    runfeat <- function(fsf) {
+      runname <- basename(fsf)
+      #system2(file.path(Sys.getenv("FSLDIR"), "bin", "feat"), fsf, stdout=file.path(dirname(fsf), paste0("feat_stdout_", runname)), stderr=file.path(dirname(fsf), paste0("feat_stderr_", runname)))
+      runFSLCommand(paste("feat", fsf), stdout=file.path(dirname(fsf), paste0("feat_stdout_", runname)), stderr=file.path(dirname(fsf), paste0("feat_stderr_", runname)))
+    }
+    #browser()
+    clusterApply(cl_fork, allFeatFiles, runfeat)
+    stopCluster(cl_fork)
+  }
+  
+}
 
 #function to setup and run afni value model
-afniValueModel <- function(mrfiles, timingdir, run=FALSE) {
+afniValueModel <- function(mrfiles, fitobj, run=FALSE) {
   #setup and run basic value model in AFNI
-  afnidir <- suppressWarnings(normalizePath(file.path(dirname(mrfiles[1L]), "../afni_value")))
+  afnidir <- file.path(normalizePath(file.path(dirname(mrfiles[1L]), "..")), "afni_value") #note: normalizePath will fail to evaluate properly if directory does not exist (e.g., dir not created yet)
+  cat("afnidir create: ", afnidir, "\n")
   dir.create(afnidir, showWarnings=FALSE) #one directory up from a given clock run
+  timingdir <- file.path(afnidir, "run_timing_deltavalue")
+  
+  d_value <- fitobj$build_design_matrix(regressors=c("clock", "feedback", "ev", "rpe_neg", "rpe_pos"), 
+      event_onsets=c("clock_onset", "feedback_onset", "clock_onset", "feedback_onset", "feedback_onset"), 
+      durations=c(0, 0, "clock_duration", "feedback_duration", "feedback_duration"), 
+      baselineCoefOrder=2, writeTimingFiles=c("AFNI"),
+      runVolumes=runlengths, output_directory=timingdir)
   
   #concat motion parameters
   motion_concat <- do.call(rbind, lapply(mrfiles, function(x)  {
@@ -78,7 +184,7 @@ afniValueModel <- function(mrfiles, timingdir, run=FALSE) {
   }
 }
 
-setwd("/Users/michael/CogEmoFaceReward/analysis")
+setwd(file.path(getMainDir(), "clock_analysis", "fmri"))
 if (!file.exists("fmri_fits")) { dir.create("fmri_fits") }
 setwd("fmri_fits")
 
@@ -95,39 +201,39 @@ posEps$add_params(
     exploreBeta()
 )
 
-library(Rniftilib) #has nice function for just reading header
+behavDir <- "/Volumes/rcn1/bea_res/Data/Tasks/EmoClockfMRI/Basic"
+fmriDir <- "/Volumes/Serena/MMClock/MR_Raw"
+behavFiles <- list.files(path=behavDir, pattern="*tcExport.csv", full.names=TRUE, recursive=TRUE)
 
 for (b in behavFiles) {
   subid <- sub("^.*fMRIEmoClock_(\\d+)_tc_tcExport.csv$", "\\1", b, perl=TRUE)
   mrfiles <- c() #force clear of mr files over subjects to avoid potential persistence from one subject to the next
   
-  if (file.exists(file.path(subid, paste0(subid, "_fitinfo.RData")))) { 
+  ##identify corresponding fmri directory
+  mrmatch <- grep(paste0(subid, "_\\d+"), list.files(fmriDir, full.names=TRUE), perl=TRUE, value=TRUE)
+  if(length(mrmatch) != 1L) {
+    warning("Unable to find fMRI directory for subid: ", subid)
+    next
+  }
+  if (! file.exists(file.path(mrmatch, "MBclock_recon"))) {
+    warning("Unable to find preprocessed data MBclock_recon for subid: ", subid)
+    next
+  }
+  
+  ##identify fmri run lengths (4th dimension)
+  mrfiles <- list.files(mrmatch, pattern="nfswukdtm_clock.*_5.nii.gz", full.names=TRUE, recursive=TRUE)
+  
+  if (length(mrfiles) == 0L) {
+    warning("Unable to find any preprocessed MB files in dir: ", mrmatch)
+    next
+  }
+  
+  runlengths <- unname(sapply(mrfiles, function(x) { nifti.image.read(x, read_data=0)$dim[4L] }))
+  
+  if (file.exists(paste0(subid, "_fitinfo.RData"))) { 
     cat("Fit data already present for: ", subid, "\n")
-    load(file.path(subid, paste0(subid, "_fitinfo.RData")))
+    load(paste0(subid, "_fitinfo.RData"))
   } else {
-    #identify corresponding fmri directory
-    mrmatch <- grep(paste0(subid, "_\\d+"), list.files(fmriDir, full.names=TRUE), perl=TRUE, value=TRUE)
-    if(length(mrmatch) != 1L) {
-      warning("Unable to find fMRI directory for subid: ", subid)
-      next
-    }
-    if (! file.exists(file.path(mrmatch, "MBclock_recon"))) {
-      warning("Unable to find preprocessed data MBclock_recon for subid: ", subid)
-      next
-    }
-    
-    #identify fmri run lengths (4th dimension)
-    mrfiles <- list.files(mrmatch, pattern="nfswuktmd_clock.*_5.nii.gz", full.names=TRUE, recursive=TRUE)
-    
-    if (length(mrfiles) == 0L) {
-      warning("Unable to find any preprocessed MB files in dir: ", mrmatch)
-      next
-    }
-    
-    runlengths <- unname(sapply(mrfiles, function(x) { nifti.image.read(x, read_data=0)$dim[4L] }))
-    
-    dir.create(subid, showWarnings=FALSE)
-    
     s <- clockdata_subject(subject_ID=subid, dataset=b)
     
     cat("Fitting behavioral data for subject: ", subid, "\n")
@@ -135,9 +241,9 @@ for (b in behavFiles) {
     #set data for model fit
     posEps$set_data(s)
     
-    incr_fit <- posEps$incremental_fit(njobs=7)
+    incr_fit <- posEps$incremental_fit(njobs=7, plot=FALSE)
     
-    png(file.path(subid, paste0(subid, "_incrfit.png")), width=9, height=6, units="in", res=300)
+    png(file.path(paste0(subid, "_incrfit.png")), width=9, height=6, units="in", res=300)
     print(incr_fit$AICplot)
     dev.off()
     
@@ -146,9 +252,7 @@ for (b in behavFiles) {
     #design matrix matching Badre et al. 2012 Neuron
     d <- f$build_design_matrix(regressors=c("mean_uncertainty", "rel_uncertainty", "rpe_pos", "rpe_neg", "rt"), 
         event_onsets=c("clock_onset", "clock_onset", "feedback_onset", "feedback_onset", "feedback_onset"), 
-        durations=c("rt", "rt", "feedback_duration", "feedback_duration", 0), baselineCoefOrder=2, writeTimingFiles="AFNI",
-        runVolumes=runlengths, output_directory=file.path(subid, "run_timing"))
-    
+        durations=c("rt", "rt", "feedback_duration", "feedback_duration", 0), baselineCoefOrder=2, runVolumes=runlengths)
     
     #delta rule value model (simple)
     vm <- deltavalue_model(clock_data=s, alphaV=0.3, betaV=0.3) #N.B. This matches V matrix from full time-clock algorithm fit.
@@ -156,14 +260,13 @@ for (b in behavFiles) {
     
     #Design with EV, clock onset, feedback_onset, PE+, PE-
     d_value <- f_value$build_design_matrix(regressors=c("clock", "feedback", "ev", "rpe_neg", "rpe_pos"), 
-        event_onsets=c("clock_onset", "feedback_onset", "feedback_onset", "feedback_onset", "feedback_onset"), 
-        durations=c(0, 0, "feedback_duration", "feedback_duration", "feedback_duration"), baselineCoefOrder=2, writeTimingFiles="AFNI",
-        runVolumes=runlengths, output_directory=file.path(subid, "run_timing_deltavalue"))
+        event_onsets=c("clock_onset", "feedback_onset", "clock_onset", "feedback_onset", "feedback_onset"), 
+        durations=c(0, 0, "clock_duration", "feedback_duration", "feedback_duration"), baselineCoefOrder=2, runVolumes=runlengths)
     
-    save(f_value, d_value, f, d, s, incr_fit, mrfiles, file=file.path(subid, paste0(subid, "_fitinfo.RData")))
+    save(f_value, d_value, f, d, s, incr_fit, mrfiles, file=paste0(subid, "_fitinfo.RData"))
   }
   
   #setup afni model
-  afniValueModel(mrfiles, timingdir=file.path(getwd(), subid, "run_timing_deltavalue"), run=FALSE)
-  
+  afniValueModel(mrfiles, f_value, run=FALSE)
+  fslValueModel(mrfiles, f_value, run=TRUE)
 }
