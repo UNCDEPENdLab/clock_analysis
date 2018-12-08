cc380bd2ca7b2a9e2896301c0ebeaab0c75bf997
Alex Dombrovski <dombrovskia@upmc.edu>
2018-12-07 19:28:00 -0500

    Within-subject H, Vmax plots

--- fmri/keuka_brain_behavior_analyses/entropy_cluster_pca.R
+++ fmri/keuka_brain_behavior_analyses/entropy_cluster_pca.R
@@ -234,6 +234,22 @@ df$v_paralimbic[df$v_f2_paralimb>0] <- 'high'
 df$low_v_fp_acc_vlpfc <- 'low'
 df$low_v_fp_acc_vlpfc[df$v_f1_neg_cog<0] <- 'high'
 
+df$learning_epoch <- 'trials 1-10'
+df$learning_epoch[df$run_trial>10] <- 'trials 11-50'
+
+
+
+
+# obtain within-subject v_max and entropy: correlated at -.37
+
+df <- df %>% group_by(id,run) %>% mutate(v_max_wi = scale(v_max),
+                                         v_entropy_wi = scale(v_entropy)
+)
+# circular, but just check to what extent each area conforms to SCEPTIC-SM: looks like there is an interaction, both need to be involved again
+ggplot(df, aes(run_trial, v_entropy_wi, color = low_h_paralimbic, lty = h_fp)) + geom_smooth(method = "loess") #+ facet_wrap(~gamma>0)
+# I think this means again that H estimates are more precise for high-paralimbic people, esp. late in learning
+ggplot(df, aes( v_entropy_wi,log(rt_swing), color = low_h_paralimbic, lty = h_fp)) + geom_smooth(method = "gam") + facet_wrap(~learning_epoch)
+ggplot(df, aes( v_max_wi,log(rt_swing), color = low_h_paralimbic, lty = h_fp)) + geom_smooth(method = "gam") + facet_wrap(~run_trial > 10)
 
 #####
 # these simple RT swing prediction models only reveal that both networks catalyze convergence regardless of condition
@@ -253,7 +269,7 @@ car::Anova(m2, '3')
 summary(m3 <- lmer(log(rt_swing) ~ (scale(-1/run_trial) + scale(v_max) + v_f1_neg_cog + v_f2_paralimb) ^3 + rewFunc + (1|id/run), df[df$rt_swing>0,]))
 car::Anova(m3, '3')
 summary(m4 <- lmer(log(rt_swing) ~ (scale(-1/run_trial) + scale(v_max) + h_f1_fp + h_f2_neg_paralimb + v_f1_neg_cog + v_f2_paralimb) ^3 + rewFunc + (1|id/run), df[df$rt_swing>0,]))
-anova(m4,m4a)
+anova(m4)
 car::Anova(m4, '3')
 # adding d introduces multi-collinearity
 summary(m5 <- lmer(log(rt_swing) ~ (scale(-1/run_trial) + scale(v_max) + h_f1_fp + h_f2_neg_paralimb + v_f1_neg_cog + v_f2_paralimb + d_f1 + d_f2 + d_f3) ^3 + rewFunc + (1|id/run), df[df$rt_swing>0,]))
@@ -336,17 +352,20 @@ anova(rt0,rtv,rtd,rth,rtvh,rtvhd) # H predicts best, each addition (V, D_AUC) im
 # but careful with cluster*cluster interactions -- high VIFs!
 
 
+# the model to rule them all?
+summary(rtwh <- lmer(scale(rt_csv) ~ (scale(run_trial) + scale(rt_vmax) + scale(rt_lag) + v_entropy_wi + v_max_wi + h_f1_fp + h_f2_neg_paralimb)^3 + rewFunc + (1|id/run), df))
+
 # plot vs. raw data
 ggplot(df, aes(rt_vmax, rt_csv, color = h_f1_fp>0)) + geom_smooth(method = "glm") + facet_wrap(~run_trial>20)
 ggplot(df, aes(rt_vmax, rt_csv, color = h_f2_neg_paralimb>0)) + geom_smooth(method = "glm") + facet_wrap(~run_trial>20)
 
 ggplot(df, aes(rt_lag, rt_csv, color = h_f1_fp>0)) + geom_smooth(method = "glm") + facet_wrap(~run_trial>20)
 
-ggplot(df, aes(rt_lag, rt_csv, color = h_f1_fp>0, lty = h_f2_neg_paralimb>0)) + geom_smooth(method = "glm")+ facet_wrap(~run_trial>20)
-ggplot(df, aes(rt_vmax, rt_csv, color = h_f1_fp>0, lty = h_f2_neg_paralimb>0)) + geom_smooth(method = "glm")+ facet_wrap(~run_trial>20)
+ggplot(df, aes(rt_lag, rt_csv, color = low_h_paralimbic, lty = h_fp)) + geom_smooth(method = "glm")+ facet_wrap(~run_trial>20)
+ggplot(df, aes(rt_vmax, rt_csv, color = low_h_paralimbic, lty = h_fp)) + geom_smooth(method = "glm")+ facet_wrap(~run_trial>20)
 
 pdf("h_timecourse_brain_fixed.pdf", width = 8, height = 8)
-ggplot(df, aes(run_trial, v_entropy, color = h_f1_fp>0, lty = h_f2_neg_paralimb>0)) + geom_smooth(method = "loess") + facet_wrap(~gamma>0)
+ggplot(df, aes(run_trial, v_entropy,color = low_h_paralimbic, lty = h_fp)) + geom_smooth(method = "loess") + facet_wrap(~gamma>0)
 dev.off()
 
 
@@ -361,7 +380,7 @@ summary(m0 <- lmer(log(rt_swing) ~ scale(-1/run_trial) * scale(v_max) * scale(v_
 summary(dm1 <- lmer(log(rt_swing) ~ (scale(-1/run_trial) + omission_lag + scale(v_max))^3 + scale(rt_lag) + (1|id/run), df[df$rt_swing>0,]))
 ggplot(na.omit(df), aes(v_entropy, rt_swing, color = v_max>25, lty = omission_lag)) + geom_smooth(method = "gam")
 
-summary(dm2 <- lmer(rt_csv ~ (scale(rt_lag) omission_lag + scale(rt_vmax))^3 + rewFunc + (1|id/run), df[df$rt_swing>0,]))
+summary(dm2 <- lmer(rt_csv ~ (scale(rt_lag) + omission_lag + scale(rt_vmax))^3 + rewFunc + (1|id/run), df[df$rt_swing>0,]))
 
 summary(dm3 <- lmer(rt_csv ~ (scale(rt_lag) + omission_lag + scale(rt_vmax) + h_f1_fp + h_f2_neg_paralimb)^3 + rewFunc + (1|id/run), df[df$rt_swing>0,]))
 
@@ -380,16 +399,6 @@ anova(dm1,dm2,dm3)
 
 summary(dm2 <- lmer(log(rt_swing) ~ (scale(-1/run_trial) +  scale(v_entropy) + omission_lag + scale(v_max) + h_f1_fp + h_f2_neg_paralimb)^3 + (1|id/run), df[df$rt_swing>0,]))
 anova(dm1,dm2)
-
-
-# obtain within-subject v_max and entropy 
-
-df <- df %>% group_by(id,run) %>% mutate(v_max_wi = scale(v_max),
-  v_entropy_wi = scale(v_entropy)
-)
-ggplot(df, aes(run_trial, v_entropy_wi, color = h_f1>0, lty = h_f2>0)) + geom_smooth(method = "loess") + facet_wrap(~gamma>0)
-ggplot(df, aes( v_entropy_wi,log(rt_swing), color = h_f1>0, lty = h_f2>0)) + geom_smooth(method = "gam") + facet_wrap(~run_trial > 10)
-ggplot(df, aes( v_max_wi,log(rt_swing), color = h_f1>0, lty = h_f2>0)) + geom_smooth(method = "gam") + facet_wrap(~run_trial > 10)
 
 summary(w1 <- lmer(log(rt_swing) ~ (scale(run_trial) + v_max_wi + v_entropy_wi)^2 + rewFunc + (1|id/run), df[df$rt_swing>0,]))
 summary(w2 <- lmer(log(rt_swing) ~ (scale(run_trial) + v_max_wi + v_entropy_wi + h_f1 + h_f2)^3 + rewFunc + (1|id/run), df[df$rt_swing>0,]))
