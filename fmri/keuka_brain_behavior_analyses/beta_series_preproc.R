15793d579c192e73e482cd211949a4b7ab6186c1
Alex Dombrovski <dombrovskia@upmc.edu>
2018-12-21 07:02:46 -0500

    OHBM abstract BS analyses

--- fmri/keuka_brain_behavior_analyses/beta_series_preproc.R
+++ fmri/keuka_brain_behavior_analyses/beta_series_preproc.R
@@ -6,7 +6,7 @@ library(lme4)
 
 remove_outliers <- function(x, na.rm = TRUE, ...) {
   qnt <- quantile(x, probs=c(.25, .75), na.rm = na.rm, ...)
-  H <- 2 * IQR(x, na.rm = na.rm)
+  H <- 1.5 * IQR(x, na.rm = na.rm)
   y <- x
   y[x < (qnt[1] - H)] <- NA
   y[x > (qnt[2] + H)] <- NA
