f8a6df5caac7f293b85163a14a565c893cef4210
Alex Dombrovski <dombrovskia@upmc.edu>
2019-07-18 18:01:11 -0400

    Raster plots of decoding and RT prediction

--- fmri/keuka_brain_behavior_analyses/hipp_decon_rt_prediction.R
+++ fmri/keuka_brain_behavior_analyses/hipp_decon_rt_prediction.R
@@ -3,15 +3,16 @@ library(modelr)
 library(tidyverse)
 library(lme4)
 library(broom)
+library(ggpubr)
 setwd("/Users/localadmin/Box/SCEPTIC_fMRI/var")
 load('feedback_hipp_tallest_by_timepoint_decon.Rdata')
 setwd('~/code/clock_analysis/fmri/keuka_brain_behavior_analyses/')
 load('trial_df_and_vhdkfpe_clusters.Rdata')
 
 # select relevant columns
-df <- df %>% select(id, run, run_trial, rewFunc,emotion, rt_csv, score_csv, rt_next, rt_vmax, v_max_wi, v_entropy_wi, v_entropy_b, v_entropy, v_max_b, Age, Female)
+df <- df %>% select(id, run, run_trial, rewFunc,emotion, rt_csv, score_csv, rt_next, rt_vmax, rt_vmax_lag,rt_vmax_change, v_max_wi, v_entropy_wi, v_entropy_b, v_entropy, v_max_b, Age, Female)
 d <- merge(df, fb_wide_t, by = c("id", "run", "run_trial"))
-d <- d %>% group_by(id, run) %>% arrange(id, run, run_trial) %>% mutate(reward = score_csv>0) %>% ungroup()
+d <- d %>% group_by(id, run) %>% arrange(id, run, run_trial) %>% mutate(reward = score_csv>0, rt_change = 100*rt_next - rt_csv) %>% ungroup()
 
 # scale decons
 scale2 <- function(x, na.rm = FALSE) (x - mean(x, na.rm = na.rm)) / sd(x, na.rm)
@@ -19,13 +20,18 @@ d <- d %>% group_by(id,run) %>%  mutate_at(vars(starts_with("hipp")), scale2, na
 # test model
 # assign("h",d$hipp_9_l_3)
 
+for (trial_cont in c("TRUE", "FALSE")) {
 newlist <- list()
+# try to predict directional RT change
 # loop over slices and timepoints
 for (slice in 1:12) {print(paste("Processing slice", slice, sep = " "))
   for (side in c("l", "r")) {
     for (t in -1:10) {
       d$h<-d[[paste("hipp", slice, side, t, sep = "_")]]
-      mf <-  lmer(rt_next ~ scale(-1/run_trial)*rewFunc + (reward + scale(rt_csv) + scale(rt_vmax) + h)^3 + (1|id/run), d)
+      if (trial_cont) {
+        mf <-  lmer(rt_next ~ scale(-1/run_trial)*rewFunc + (reward + scale(rt_csv) + scale(rt_vmax_lag) + scale(rt_vmax) + h)^3 + (1|id/run), d)}
+      else {
+        mf <-  lmer(rt_next ~ (reward + scale(rt_csv) + scale(rt_vmax_lag) + h)^3 + (1|id/run), d)}
       dm <- tidy(mf)
       dm$slice <- slice
       dm$side <- side
@@ -36,50 +42,97 @@ for (slice in 1:12) {print(paste("Processing slice", slice, sep = " "))
 }
 bdf <- do.call(rbind,newlist)
 bdf$slice <- as.factor(bdf$slice)
+terms <- names(fixef(mf))
+if (trial_cont) {
+  setwd('~/OneDrive/collected_letters/papers/sceptic_fmri/hippo/figs')
+}
+else {
+setwd('~/OneDrive/collected_letters/papers/sceptic_fmri/hippo/figs/no_trial_contingency/')}
 
-# scale(-1/run_trial)                     -1.17674    7.12535  -0.165
-# rewFuncCEVR                             -1.57670    3.09577  -0.509
-# rewFuncDEV                              -1.12577    3.21316  -0.350
-# rewFuncIEV                               0.08451    2.99803   0.028
-# rewardTRUE                               0.05778    0.69452   0.083
-# scale(rt_csv)                            0.74853    0.51851   1.444
-# scale(rt_vmax)                           5.10485    0.53177   9.600
-# h                                       -0.29986    0.57068  -0.525
-# scale(-1/run_trial):rewFuncCEVR          3.63437    8.11871   0.448
-# scale(-1/run_trial):rewFuncDEV           2.08506    8.06810   0.258
-# scale(-1/run_trial):rewFuncIEV           1.61538    7.45955   0.217
-# rewardTRUE:scale(rt_csv)                 0.89619    0.64330   1.393
-# rewardTRUE:scale(rt_vmax)                1.41641    0.64746   2.188
-# rewardTRUE:h                             0.56961    0.66931   0.851
-# scale(rt_csv):scale(rt_vmax)             0.72352    0.35888   2.016
-# scale(rt_csv):h                          0.46836    0.61410   0.763
-# scale(rt_vmax):h                        -0.61496    0.60614  -1.015
-# rewardTRUE:scale(rt_csv):scale(rt_vmax) -1.01659    0.46498  -2.186
-# rewardTRUE:scale(rt_csv):h              -0.42836    0.78961  -0.542
-# rewardTRUE:scale(rt_vmax):h              0.83799    0.77683   1.079
-# scale(rt_csv):scale(rt_vmax):h          -0.07784    0.28449  -0.274
-
-edf <- bdf %>% filter(term == "scale(rt_vmax):h" & t < 8)
-ggplot(edf, aes(t, estimate, color = slice)) + geom_line() + 
+for (fe in terms)
+{edf <- bdf %>% filter(term == paste(fe) & t < 8)
+p1 <- ggplot(edf, aes(t, estimate, color = slice)) + geom_line() + 
   geom_errorbar(aes(ymin = estimate - std.error, ymax = estimate + std.error), alpha = .5) + facet_wrap(~side) + 
-  theme_dark() + scale_color_viridis_d() + geom_hline(yintercept = 0, lty = "dashed", color = "red")
+  theme_dark() + scale_color_viridis_d() + geom_hline(yintercept = 0, lty = "dashed", color = "red") + labs(title = paste(fe))
 
-ggplot(edf, aes(t, slice)) + geom_tile(aes(fill = estimate))
+p2 <- ggplot(edf, aes(t, slice)) + geom_tile(aes(fill = estimate, alpha = abs(statistic)>2), size = 1) + facet_wrap(~side) + 
+  scale_fill_viridis(option = "plasma") + scale_color_grey() + labs(title = paste(fe))
+
+termstr <- str_replace_all(fe, "[^[:alnum:]]", "_")
+pdf(paste(termstr, ".pdf", sep = ""), width = 12, height = 12)
+print(ggarrange(p1,p2,ncol = 1, nrow = 2))
+dev.off()
+}
+}
+# "decoding" analyses
+# look at absolute change
+for (trial_cont in c("TRUE", "FALSE")) {
+newlist <- list()
+# loop over slices and timepoints
+for (slice in 1:12) {print(paste("Processing slice", slice, sep = " "))
+  for (side in c("l", "r")) {
+    for (t in -1:10) {
+      d$h<-d[[paste("hipp", slice, side, t, sep = "_")]]
+      if (trial_cont) {
+        md <-  lmer(h ~ scale(-1/run_trial)*rewFunc + reward + scale(rt_csv) + scale(rt_vmax_lag) + scale(rt_vmax_change) + v_entropy_wi + (1|id), d)
+        } else {
+        md <-  lmer(h ~ reward + scale(rt_csv) + scale(rt_vmax_lag) + scale(rt_vmax_change) +  v_entropy_wi + (1|id), d)}
+      dm <- tidy(md)
+      dm$slice <- slice
+      dm$side <- side
+      dm$t <- t
+      dm <- dm %>% mutate(stat_order = as.factor(case_when(abs(statistic) < 2 ~ '1', 
+                                                 abs(statistic) > 2 & abs(statistic) < 3 ~ '2', 
+                                                 abs(statistic) > 3 ~ '3')))
+      newlist[[paste("hipp", slice, side, t, sep = "_")]]<-dm
+    }
+  }
+}
+ddf <- do.call(rbind,newlist)
+ddf$slice <- as.factor(ddf$slice)
+ddf$stat_order <- factor(ddf$stat_order, labels = c("NS", "|t| > 2", "|t| > 3"))
+# terms <- names(fixef(md))
+terms <- names(md$coefficients)
+if (trial_cont) {
+  setwd('~/OneDrive/collected_letters/papers/sceptic_fmri/hippo/figs/decode')
+} else {
+  setwd('~/OneDrive/collected_letters/papers/sceptic_fmri/hippo/figs/decode/no_trial_contingency/')}
 
 
-mf <-  lmer(rt_next ~ scale(-1/run_trial) * scale(rt_csv) +  scale(rt_csv)  + reward * scale(rt_csv)*hipp_1_r_5 + (1|id/run), d)
-summary(mf)
-car::Anova(mf)
-car::vif(mf)
-vif.lme(mf)
-mh <- lmer(hipp_9_r_4 ~ (scale(-1/run_trial) + scale(rt_csv) + reward + 
-                           v_entropy_wi)^2 + v_max_b + v_entropy_b + (1|id/run), d) 
-summary(mh)
+for (fe in terms)
+{edf <- ddf %>% filter(term == paste(fe) & t < 8)
+# p1 <- ggplot(edf, aes(t, estimate, color = slice)) + geom_line() + 
+#   geom_errorbar(aes(ymin = estimate - std.error, ymax = estimate + std.error), alpha = .5) + facet_wrap(~side) + 
+#   theme_dark() + scale_color_viridis_d() + geom_hline(yintercept = 0, lty = "dashed", color = "red")
 
 
-mb3 <-  lmer(rt_next ~ (scale(-1/run_trial) + scale(rt_csv) + scale(rt_vmax) + reward + 
-                          hipp_9_r_5)^3 +  (1|id/run), d)
 
-summary(mb3)
-car::Anova(mb)
-car::vif(mb)
+termstr <- str_replace_all(fe, "[^[:alnum:]]", "_")
+pdf(paste(termstr, ".pdf", sep = ""), width = 12, height = 7)
+# print(ggplot(edf, aes(t, slice)) + geom_tile(aes(fill = estimate, alpha = abs(statistic)>2), size = 1) + facet_wrap(~side) + 
+print(ggplot(edf, aes(t, slice)) + geom_tile(aes(fill = estimate, alpha = stat_order), size = 1) + facet_wrap(~side) + 
+  scale_fill_viridis(option = "plasma") + scale_color_grey() + labs(title = paste(fe)))
+# print(ggarrange(p2,ncol = 1, labels = paste(fe), vjust = 4, font.label = list(color = "black", size = 16)))
+dev.off()
+}
+}
+
+
+
+
+# mf <-  lmer(rt_next ~ scale(-1/run_trial) * scale(rt_csv) +  scale(rt_csv)  + reward * scale(rt_csv)*hipp_1_r_5 + (1|id/run), d)
+# summary(mf)
+# car::Anova(mf)
+# car::vif(mf)
+# vif.lme(mf)
+# mh <- lmer(hipp_9_r_4 ~ (scale(-1/run_trial) + scale(rt_csv) + reward + 
+#                            v_entropy_wi)^2 + v_max_b + v_entropy_b + (1|id/run), d) 
+# summary(mh)
+# 
+# 
+# mb3 <-  lmer(rt_next ~ (scale(-1/run_trial) + scale(rt_csv) + scale(rt_vmax) + reward + 
+#                           hipp_9_r_5)^3 +  (1|id/run), d)
+# 
+# summary(mb3)
+# car::Anova(mb)
+# car::vif(mb)
