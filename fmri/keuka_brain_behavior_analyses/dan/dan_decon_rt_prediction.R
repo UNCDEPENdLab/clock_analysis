9e491cc7bb6e9bf0fdb8c82ecb6749ee437639b8
Alex Dombrovski <dombrovskia@upmc.edu>
2020-11-18 00:50:14 -0500

    DAN MEDUSAs

---
+++ fmri/keuka_brain_behavior_analyses/dan/dan_decon_rt_prediction.R
@@ -0,0 +1,227 @@
+# with 'decode = T' makes MEDUSA decoding plots for Fig. 4 E-G.
+# loops over decoding and uncertainty prediction multi-level models for various hippocampal slices and post-feedback time points
+# first run medusa_event_locked_lmer.R
+
+library(modelr)
+library(tidyverse)
+library(lme4)
+library(afex)
+library(broom)
+library(broom.mixed) #plays will with afex p-values in lmer wrapper
+library(ggpubr)
+library(car)
+library(viridis)
+
+# select data
+smooth_in_mask = T  # main analysis: data smoothed within mask
+unsmoothed = F      # no smoothing whatsoever
+newmask = F         # sensivitivy analysis: restrictive COBRA mask (default: Harvard-Oxford)
+
+# what to run
+plots = T
+decode = T  # main analysis for Fig. 4 E-G
+u = F       # exploratory analysis attempting to predict the uncertainty of the next choice
+
+
+# load data
+setwd('~/Box/SCEPTIC_fMRI/dan_medusa/cache/')
+load('feedback_dan_wide_ts.Rdata')
+setwd('~/code/clock_analysis/fmri/keuka_brain_behavior_analyses/')
+
+# read in behavioral data
+load('trial_df_and_vh_pe_clusters_u.Rdata')
+cache_dir <- "~/Box/SCEPTIC_fMRI/dan_medusa/cache/"
+repo_dir <- "~/code/clock_analysis"
+
+# select relevant columns for compactness
+df <- df %>% select(id, run, run_trial, rewFunc,emotion, last_outcome, rt_csv, score_csv, rt_next, pe_max, rt_vmax, rt_vmax_lag,
+                    rt_vmax_change, v_max_wi, v_entropy_wi, v_entropy_b, v_entropy, v_max_b, u_chosen_quantile, u_chosen_quantile_lag, u_chosen_quantile_change, 
+                    rt_vmax_lag_sc, rt_lag_sc, rt_csv_sc, trial_neg_inv_sc, Age, Female)
+
+# add deconvolved timeseries
+d <- merge(df, fb_wide, by = c("id", "run", "run_trial"))
+d <- d %>% group_by(id, run) %>% arrange(id, run, run_trial) %>% mutate(rt_change = 100*rt_next - rt_csv, 
+                                                                        v_entropy_wi_lead = lead(v_entropy_wi),
+                                                                        v_entropy_wi_change = v_entropy_wi_lead-v_entropy_wi,
+                                                                        u_chosen_quantile_next = lead(u_chosen_quantile),
+                                                                        u_chosen_quantile_change_next = lead(u_chosen_quantile_change),
+                                                                        outcome = lead(last_outcome)) %>% ungroup()
+
+scale2 <- function(x, na.rm = FALSE) (x - mean(x, na.rm = na.rm)) / sd(x, na.rm)
+# scale decon across subjects as a predictor
+# choice uncertainty prediction analyses run on scaled 'ds' dataframe instead of 'd'
+ds <- d %>% mutate_at(vars(starts_with("dan")), scale2, na.rm = TRUE) %>% ungroup()
+
+
+#######
+# "decoding" analyses
+# combined right and left hippocampus with side as a predictor
+# if model does not converge, update with new starting values
+labels <- names(d[grepl("_R_|_r_|_L_|_l_", names(d))])
+if (decode) {
+  newlist <- list()
+  for (label in labels) {print(paste("Processing parcel", stringr::str_remove(pattern = gsub(".*_", "\\1", label), label),
+                                                                              ", time", gsub(".*_", "\\1", label),  sep = " "))
+    # for (side in c("l", "r")) {
+     # for (t in -1:10) {
+    d$h<-d[[label]]
+    # d$h<-d[[paste(stringr::str_remove(pattern = gsub(".*_", "\\1", label), label), t, sep = "")]]
+    md <-  lmer(h ~ trial_neg_inv_sc + scale(rt_csv) + scale(rt_vmax_lag)  + scale(rt_vmax_change)  + v_entropy_wi  + v_max_wi + #u_chosen_quantile_change +
+                  (1|id), d, control=lmerControl(optimizer = "nloptwrap"))
+    while (any(grepl("failed to converge", md@optinfo$conv$lme4$messages) )) {
+      print(md@optinfo$conv$lme4$conv)
+      ss <- getME(md,c("theta","fixef"))
+      md <- update(md, start=ss)}
+    
+    dm <- tidy(md)
+    dm$label <- label
+    # dm$side <- side
+    dm$t <- gsub(".*_", "\\1", label)
+    dm <- dm %>% mutate(stat_order = as.factor(case_when(abs(statistic) < 2 ~ '1', 
+                                                         abs(statistic) > 2 & abs(statistic) < 3 ~ '2', 
+                                                         abs(statistic) > 3 ~ '3')),
+                        p_value = as.factor(case_when(p.value > .05 ~ '1',
+                                                      p.value < .05 & p.value > .01 ~ '2',
+                                                      p.value < .01 & p.value > .001 ~ '3',
+                                                      p.value <.001 ~ '4'))
+    )
+    newlist[[label]]<-dm
+    # }
+     }
+  }
+  ddf <- do.call(rbind,newlist)
+  ddf$t <- as.numeric(ddf$t)
+  ddf$label <- as.factor(stringr::str_remove(pattern = paste0("_", gsub(".*_", "\\1", ddf$label), ""), ddf$label))
+  ddf$stat_order <- factor(ddf$stat_order, labels = c("NS", "|t| > 2", "|t| > 3"))
+  ddf$p_value <- factor(ddf$p_value, labels = c("NS", "p < .05", "p < .01", "p < .001"))
+  
+  terms <- names(fixef(md))
+  # terms <- names(md$coefficients)
+  # FDR
+  ddf <- ddf  %>% group_by(term) %>% mutate(p_fdr = p.adjust(p.value, method = 'fdr'),
+                                            p_level_fdr = as.factor(case_when(
+                                              # p_fdr > .1 ~ '0',
+                                              # p_fdr < .1 & p_fdr > .05 ~ '1',
+                                              p_fdr > .05 ~ '1',
+                                              p_fdr < .05 & p_fdr > .01 ~ '2',
+                                              p_fdr < .01 & p_fdr > .001 ~ '3',
+                                              p_fdr <.001 ~ '4'))#,
+                                            # side_long = case_when(side=='l' ~ 'Left',
+                                            #                       side=='r' ~ 'Right')
+  )
+  
+  ddf$p_level_fdr <- factor(ddf$p_level_fdr, levels = c('1', '2', '3', '4'), labels = c("NS","p < .05", "p < .01", "p < .001"))
+  
+  ddf$`p, FDR-corrected` = ddf$p_level_fdr
+  
+  ##############
+  # make plots
+  ##############
+  setwd('~/OneDrive/collected_letters/papers/sceptic_fmri/dan/plots/')
+  for (fe in terms) {
+    edf <- ddf %>% filter(term == paste(fe) & t < 8) 
+    termstr <- str_replace_all(fe, "[^[:alnum:]]", "_")
+    pdf(paste(termstr, ".pdf", sep = ""), width = 10, height = 12)
+    print(ggplot(edf, aes(t, label)) + geom_tile(aes(fill = estimate, alpha = `p, FDR-corrected`), size = 1) +  
+            scale_fill_viridis(option = "plasma") + scale_color_grey() + xlab("Time after outcome, seconds") + ylab("Parcel") + 
+            labs(alpha = expression(~italic(p)~', FDR-corrected'))) 
+    dev.off()
+  }
+  
+  
+  
+  ########
+  # choice uncertainty prediction analyses
+  # diagnose trial/h collinearity
+  # ggplot(fb_comb, aes(run_trial, decon_interp, color = as.factor(bin_center))) + geom_smooth(method = 'gam', se = F)
+  # drop models without contingency and trial
+  # running with h scaled
+  
+  # to think more about it, all behavioral variables should be represented in the brain
+  # what is represented is addressed in decoding analyses
+  # that said, it seems that uncertainty prediction analyses yield negative results over a range of models
+  
+  # uncertainty prediction analyses also run on scaled data
+  if (u) {
+    # for (trial_cont in c("TRUE", "FALSE")) {
+    # for (trial_cont in c("FALSE")) {
+    newlist <- list()
+    for (slice in 1:9) {print(paste("Processing slice", slice, sep = " "))
+      # for (side in c("l", "r")) {
+      for (t in 0:10) {
+        d$h<-d[[paste("vmPFC", slice, t, sep = "_")]]
+        # if (trial_cont) {
+        uf <- lmer(u_chosen_quantile_next ~  h * scale(run_trial) + u_chosen_quantile + (1|id/run), d)
+        # else {
+        # mf <-  lme4::lmer(rt_next ~ (scale(pe_max) + scale(rt_csv) + scale(rt_vmax_lag) + scale(rt_vmax_change) + scale(v_entropy_wi) + h)^2 + (1|id/run), ds)
+        # uf <- lmer(u_chosen_next ~ scale(-1/run_trial)*scale(h) + scale(rt_csv)*scale(h) + scale(rt_vmax)*scale(h) + last_outcome*scale(h) + v_entropy_wi*scale(h) + scale(u_chosen) + (1|id/run), ds) 
+        # }
+        dm <- broom.mixed::tidy(uf,effects = "fixed") %>% mutate(term = str_remove(term, "TRUE")) # make betas compatible with ANOVA
+        # run without anova
+        # an <- broom.mixed::tidy(car::Anova(uf, '3')) %>% rename(anova_p = p.value, chisq = statistic)
+        dm$slice <- slice
+        # dm$side <- side
+        dm$t <- t
+        # dm <- inner_join(dm, an, by = "term") # this only works for continuous terms, unfortunately
+        dm <- dm %>% mutate(stat_order = as.factor(case_when(abs(statistic) < 2 ~ '1', 
+                                                             abs(statistic) > 2 & abs(statistic) < 3 ~ '2', 
+                                                             abs(statistic) > 3 ~ '3')),
+                            p_value = as.factor(case_when(p.value > .05 ~ '1',
+                                                          p.value < .05 & p.value > .01 ~ '2',
+                                                          p.value < .01 & p.value > .001 ~ '3',
+                                                          p.value <.001 ~ '4')))
+        # newlist[[paste("hipp", slice, side, t, sep = "_")]]<-dm
+        newlist[[paste("hipp", slice, t, sep = "_")]]<-dm
+      }
+      # }
+    }
+    bdf <- do.call(rbind,newlist)
+    bdf$slice <- as.factor(bdf$slice)
+    bdf$stat_order <- factor(bdf$stat_order, labels = c("NS", "|t| > 2", "|t| > 3"))
+    bdf <- bdf  %>% group_by(term) %>% mutate(p_fdr = p.adjust(p.value, method = 'fdr'),
+                                              p_level_fdr = as.factor(case_when(
+                                                # p_fdr > .1 ~ '0',
+                                                # p_fdr < .1 & p_fdr > .05 ~ '1',
+                                                p_fdr > .05 ~ '1',
+                                                p_fdr < .05 & p_fdr > .01 ~ '2',
+                                                p_fdr < .01 & p_fdr > .001 ~ '3',
+                                                p_fdr <.001 ~ '4'))#,
+                                              # side_long = case_when(side=='l' ~ 'Left',
+                                              #                       side=='r' ~ 'Right')
+    )
+    
+    # ddf$p_level_fdr <- factor(ddf$p_level_fdr, labels = c("NS","p < .1", "p < .05", "p < .01", "p < .001"))
+    bdf$p_level_fdr <- factor(bdf$p_level_fdr, labels = c("NS","p < .05", "p < .01", "p < .001"))
+    
+    bdf$`p, FDR-corrected` = bdf$p_level_fdr
+    
+    # sanity check for FDR-corrected p value labels
+    ggplot(bdf, aes(p_level_fdr, p_fdr)) + geom_point()
+    
+    terms <- unique(bdf$term)
+    # if (trial_cont) {
+    if (unsmoothed) {setwd('~/OneDrive/collected_letters/papers/sceptic_fmri/hippo/figs/u_predict/unsmoothed')
+      # } else {setwd('~/OneDrive/collected_letters/papers/sceptic_fmri/hippo/figs/newmask/u_predict')}
+    } else {setwd('~/OneDrive/collected_letters/papers/sceptic_fmri/hippo/figs/u_predict')}
+    
+    # else {
+    # if (unsmoothed) {setwd('~/OneDrive/collected_letters/papers/sceptic_fmri/hippo/figs/u_predict/unsmoothed/no_trial_contingency')
+    # } else {setwd('~/OneDrive/collected_letters/papers/sceptic_fmri/hippo/figs/newmask/u_predict/no_trial_contingency/')}}
+    setwd('~/OneDrive/collected_letters/papers/sceptic_fmri/vmPFC/figs/u_predict')
+    if (plots) {
+      for (fe in terms) 
+      {
+        edf <- bdf %>% filter(term == paste(fe) & t < 8) 
+        termstr <- str_replace_all(fe, "[^[:alnum:]]", "_")
+        pdf(paste(termstr, "_vmPFC.pdf", sep = ""), width = 5, height = 3.5)
+        print(ggplot(edf, aes(t, slice)) + geom_tile(aes(fill = estimate, alpha = `p, FDR-corrected`), size = 1) +  
+                scale_fill_viridis(option = "plasma") + scale_color_grey() + xlab("Time after outcome, seconds") + ylab("Posterior <-- Location --> Anterior\n (9 slices)") + 
+                labs(alpha = expression(~italic(p)~', FDR-corrected'))) 
+        dev.off()
+      }
+    }
+  }
+  # }
+  
+  
+  
---
+++ fmri/keuka_brain_behavior_analyses/dan/medusa_event_locked_lmer_dan.R
@@ -0,0 +1,187 @@
+##  updated MEDUSA lmer scripts with only the newest plots 
+#   for ramps (Fig. 3) and post-feedback responses (Fig. 4 A-D)
+#   by running 'load_medusa_data.R' this also prepares data for
+#   decoding analyses (Fig. 4 E-G)
+library(dplyr)
+library(tidyverse)
+library(psych)
+library(ggcorrplot)
+library(lme4)
+library(ggpubr)
+library(cowplot)
+# library(sjPlot)
+# library(sjmisc)
+library(ggeffects)
+library(mlVAR)
+library(viridis)
+library(car)
+library(data.table)
+library(emmeans)
+library(wesanderson)
+source('~/code/Rhelpers/screen.lmerTest.R')
+source('~/code/Rhelpers/vif.lme.R')
+
+#####################
+
+# select data
+smooth_in_mask = T  # main analysis: data smoothed within mask
+unsmoothed = F      # no smoothing whatsoever
+newmask = F         # sensivitivy analysis: restrictive COBRA mask (default: Harvard-Oxford)
+reprocess = T
+
+repo_directory <- "~/code/clock_analysis"
+#repo_directory <- "~/Data_Analysis/clock_analysis"
+
+#load the data, and reprocess if requested
+source(file.path(repo_directory, "fmri/keuka_brain_behavior_analyses/dan/load_medusa_data_dan.R"))
+setwd('~/OneDrive/collected_letters/papers/sceptic_fmri/dan/plots/')
+
+##############################
+# overall DAN response to reinforcement by trial
+fb_comb$trial_neg_inv_sc = scale(-1/fb_comb$run_trial)
+# fb_comb$bin6_f = as.factor(fb_comb$bin6)
+fb_comb <- fb_comb %>% mutate(trial_f = as.factor(round((run_trial + 10)/12,digits = 0))) # also a 6-bin version
+tm1 <- lmer(decon_interp ~ (label + evt_time_f + trial_f)^2 + scale(rt_csv)*evt_time_f + (1 | id/run), fb_comb %>% filter (iti_prev>1 & iti_ideal>8 & evt_time < 9))
+while (any(grepl("failed to converge", tm1@optinfo$conv$lme4$messages) )) {
+  print(tm1@optinfo$conv$lme4$conv)
+  ss <- getME(tm1,c("theta","fixef"))
+  tm1 <- update(tm1, start=ss)}
+
+summary(tm1)
+vif.lme(tm1)
+Anova(tm1, '3')
+# emt <- as.data.frame(emmeans(tm1,specs = c("trial_neg_inv_sc", "bin_center_z"), at = list(bin_center_z = c(-2,-1, 0, 1,2), trial_neg_inv_sc = c(-2,-1, 0, 1,2))))
+# emt <- as.data.frame(emmeans(tm1,specs = c("trial_neg_inv_sc", "bin6_f"), at = list(trial_neg_inv_sc = c(-2,-1, 0, 1,2))))
+emt <- as.data.frame(emmeans(tm1,specs = c("trial_f", "label")))
+
+emt <- emt %>% mutate(`DAN response` = emmean, epoch = case_when(
+  trial_f == 1 ~ '0-10',
+  trial_f == 2 ~ '11-20',
+  trial_f == 3 ~ '21-30',
+  trial_f == 4 ~ '31-40',
+  trial_f == 5 ~ '41-50',
+))
+# Fig. 4D
+#pdf("../supp/supp4d_trial_hipp_AH_PH_bin6_f.pdf", width = 3, height = 3)
+
+# ggplot(rtvmax_comb,aes(run_trial,decon_interp, color = axis_bin, lty = reward)) + geom_smooth(method = "gam", formula = y ~ splines::ns(x,3),  se = F) + scale_color_viridis_d() + theme_dark()
+fig4d <- ggplot(emt, aes(epoch, `DAN response`, color = label, group = label)) + 
+  # geom_errorbar(aes(ymin = asymp.LCL, ymax = asymp.UCL),position = position_dodge(width = .5), size = .5) + geom_line(size = 1.5,position = position_dodge(width = .5)) +  theme(legend.position = "none") +
+  # geom_linerange(aes(ymin = asymp.LCL, ymax = asymp.UCL),position = position_dodge(width = .5), size = .5, color = "grey80") +  geom_line(size = 1.5,position = position_dodge(width = .5)) +  theme(legend.position = "none") +
+  geom_line(size = 1,position = position_dodge(width = .5)) + 
+  geom_linerange(aes(ymin = emmean - SE, ymax = emmean + SE), position = position_dodge(width = .5), size = .5, color = "grey80") +
+  geom_point(position = position_dodge(width = .5), size = .5, color = "black", shape=1) +
+  xlab('Trial (binned)') + ylab('DAN response (AU)') +
+  # scale_color_gradientn(colors = pal, guide = 'none') +  theme(legend.position = "none") +
+  theme_bw(base_size=13) +
+  theme(legend.title = element_blank(),
+        panel.grid.major = element_line(colour = "grey45"), 
+        panel.grid.minor = element_line(colour = "grey45"), 
+        panel.background = element_rect(fill = 'grey40'),
+        axis.title.y = element_text(margin=margin(r=6)),
+        axis.title.x = element_text(margin=margin(t=6)),
+        axis.text.x = element_text(size=10)) #condense further in illustrator
+
+ggsave("../4d_trial_DAN.pdf", fig4d, width = 8, height = 6, useDingbats=FALSE)
+
+# post-hoc for trial * bin
+em <- emmeans(tm1,specs = c("trial_f", "label"))
+pwpp(em, sort = FALSE, method = "trt.vs.ctrl1", type = "response", side = ">")
+CLD = emmeans::CLD(em,
+          alpha=0.05,
+          Letters=letters,
+          adjust="tukey")
+
+
+
+pdf("4c_fb_DAN_trial.pdf", width = 6, height = 6)
+#pdf("../supp/supp4c_fb_hipp_AP_trial_anderson.pdf", width = 3, height = 3)
+
+ggplot(fb_comb %>% filter (iti_prev>1 & iti_ideal>8 & evt_time < 9), aes(run_trial, decon_interp, color = label, group = label)) + 
+  geom_smooth(method = "gam", formula = y~splines::ns(x,3),  se = F, size = 1.2) + 
+  # scale_color_gradientn(colors = pal, guide = 'none') + 
+  xlab('Trial') + ylab('DAN response (AU)') +
+  theme_bw(base_size=13) +
+  theme(legend.title = element_blank(),
+        panel.grid.major = element_line(colour = "grey45"), 
+        panel.grid.minor = element_line(colour = "grey45"), 
+        panel.background = element_rect(fill = 'grey40'),
+        axis.title.y = element_text(margin=margin(r=6)),
+        axis.title.x = element_text(margin=margin(t=6))) +
+  scale_y_continuous(breaks=c(0.49, 0.50, 0.51))
+dev.off()
+
+
+
+# check that it's specific to entropy and not last reward
+# it is, but PH is also more active after omissions and AH, after rewards
+rm3 <- lmer(decon_interp ~ (evt_time + bin_center_z + reward_lag + side) ^2 + (evt_time_sq + bin_center_z + reward_lag + side) ^2 + decon_prev_z + reward_lag + scale(rt_csv) +  (1 | id/run), rvdf)
+summary(rm3)
+vif.lme(rm3)
+Anova(rm3, '3')
+g <- ggpredict(rm3, terms = c("evt_time","bin_center_z [-2,-1, 0, 1,2]", "reward_lag"))
+g <- plot(g, facet = F, dodge = .3)
+g + scale_color_viridis_d(option = "plasma") + theme_dark()
+
+anova(rm1,rm2, rm3)
+
+
+#### final descriptive model -- remove swing
+ee6 <- lmer(decon_interp ~ label*evt_time_f*reward + scale(rt_csv)*evt_time_f + (1 | id/run) + (1 | side), fb_comb %>% filter (iti_prev>1 & iti_ideal>8 & evt_time < 9))
+summary(ee6)
+car::Anova(ee6, '3')
+vif.lme(ee6)
+em6 <- as.data.frame(emmeans(ee6, "label", by = c( "evt_time_f", "reward"), at = list( bin_center_z = c(-2,-1, 0, 1, 2)))) %>% 
+  mutate(reward_text = case_when(
+    reward == 'reward' ~ 'Reward',
+    reward == 'omission' ~ 'Omission'
+  ))
+# ggplot(em6, aes(evt_time_f, emmean, color = bin_center_z)) + 
+#   geom_point() + geom_errorbar(aes(ymin = asymp.LCL, ymax = asymp.UCL)) + facet_wrap(.~reward) + scale_color_viridis() + theme_dark()
+
+#######
+# Fig. 4B (post-feedback MEDUSA)
+# 
+# setwd('~/OneDrive/collected_letters/papers/sceptic_fmri/hippo/figs/final/fig_4')
+# pal = wes_palette("Zissou1", 24, type = "continuous")
+fig4b <- ggplot(em6, aes(as.numeric(evt_time_f), emmean, color = label, group = label)) + 
+  # geom_point(position = position_dodge2(width = 1)) + geom_errorbar(aes(ymin = asymp.LCL, ymax = asymp.UCL),position = position_dodge2(width = .2)) + geom_line(position = position_dodge2(width = 1)) + facet_wrap(.~reward_text) +
+  geom_line(position = position_dodge2(width = 1), size = 1) + facet_wrap(.~reward_text) +
+  geom_linerange(aes(ymin = emmean - SE, ymax = emmean + SE),position = position_dodge2(width = 1), color = 'grey80') + 
+  geom_point(position = position_dodge(width = 1), size = .5, color = "black", shape=1) +
+  # scale_color_gradientn(colors = pal, guide = 'none') + 
+  xlab("Time after feedback (seconds)") + ylab("DAN response (AU)") +
+  theme_bw(base_size=13) +
+  theme(legend.title = element_blank(),
+        panel.grid.major = element_line(colour = "grey45"), 
+        panel.grid.minor = element_line(colour = "grey45"), 
+        panel.background = element_rect(fill = 'grey40'),
+        axis.title.y = element_text(margin=margin(r=6)),
+        axis.title.x = element_text(margin=margin(t=6))) +
+  scale_x_continuous(breaks=c(0, 2, 4, 6, 8, 10))
+
+ggsave('4b_medusa_feedback_DAN_reward.pdf', fig4b, height = 5, width = 8, useDingbats=FALSE)
+
+# smoothed raw data
+fb_comb <- fb_comb %>%  mutate(reward_text = case_when(
+  reward == 'reward' ~ 'Reward',
+  reward == 'omission' ~ 'Omission'
+))
+
+pdf('4a_medusa_feedback_DAN_reward_raw_smoothed.pdf', height = 5, width = 8)
+ggplot(fb_comb %>% filter (iti_prev>1 & iti_ideal>8 & evt_time < 9), aes(as.numeric(evt_time_f), decon_interp, group = label, color = label)) + 
+  geom_smooth(method = "gam", formula = y~splines::ns(x,3), se = F, size = .75) +   
+  # scale_color_gradientn(colors = pal, guide = 'none') + 
+  xlab("Time after feedback (seconds)") + ylab("DAN response (AU)") +
+  theme_bw(base_size=13) +
+  theme(legend.title = element_blank(),
+        panel.grid.major = element_line(colour = "grey45"), 
+        panel.grid.minor = element_line(colour = "grey45"), 
+        panel.background = element_rect(fill = 'grey40'),
+        axis.title.y = element_text(margin=margin(r=6)),
+        axis.title.x = element_text(margin=margin(t=6))) + facet_wrap(~reward_text) +
+  scale_x_continuous(breaks=c(0, 2, 4, 6, 8, 10))
+
+dev.off()
+
+
