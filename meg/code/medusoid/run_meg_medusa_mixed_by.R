0f956f005c6bef0cb5b0b72b5d49597ad4daeff4
Alex Dombrovski <dombrovskia@upmc.edu>
2021-05-11 10:59:28 -0400

    Combined sensors: tweaks.

--- meg/code/medusoid/run_meg_medusa_mixed_by.R
+++ meg/code/medusoid/run_meg_medusa_mixed_by.R
@@ -1,15 +1,18 @@
 # not functional for TF yet
 
 # run to avoid feeding extra data to workers
-for (i in 1:6) {
+rm(list=ls())
+# crashes on 4, Right Parietal
+# rerun starting after it
+# for (i in 1:6) {
+for (i in 4) {
   Sys.setenv(filenum = i)
-  rm(list=ls())
   gc()
   # call, options
-  Sys.setenv(encode = T, rt_predict = T, alignment = c("RT"), domain = "tf", group_sensors = F)
+  Sys.setenv(encode = F, rt_predict = T, alignment = c("RT"), domain = "tf", group_sensors = F)
   source("~/code/clock_analysis/meg/code/medusoid/meg_medusa_mixed_by_combined.R")
   
   gc()
-  Sys.setenv(encode = T, rt_predict = T, alignment = c("clock"), domain = "tf", group_sensors = Fi)
+  Sys.setenv(encode = F, rt_predict = F, alignment = c("clock"), domain = "tf", group_sensors = F)
   source("~/code/clock_analysis/meg/code/medusoid/meg_medusa_mixed_by_combined.R")
 }
