dff7b327719eb75695942f15d68276dcfbbe9146
ayd1 <ayd1@login0.crc.pitt.edu>
2021-06-24 16:50:15 -0400

    Setup for whole-brain MEG analyses

---
+++ meg/code/medusoid/sbatch_scripts/sbatch_meg_mixed_wholebrain.bash
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#SBATCH -p smp
+#SBATCH -N 1
+#SBATCH --mem 64g
+#SBATCH -n 1
+#SBATCH -t 23:00:00
+#SBATCH --mail-user=ayd1@pitt.edu
+#SBATCH --mail-type=ALL
+#SBATCH -c 32
+[ -z "$sourcefilestart" ] && echo "No sourcefilestart env variable passed in" && exit 1
+[ -z "$incrementby" ] && echo "No incrementby env variable passed in" && exit 1
+[ -z "$epoch" ] && echo "No epoch env variable passed in" && exit 1
+
+#fid=$( basename "$sourcefile" )
+#export filename=$fid
+
+cd ../ #sbatch scripts live in subdirectory
+R CMD BATCH --no-save --no-restore meg_medusa_mixed_by_wholebrain.R time_freq_${epoch}_${sourcefilestart}_$(date +%s).Rout
