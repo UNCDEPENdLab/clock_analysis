af0c2459f4767e214ede88f61b108274af00ba76
Alex Dombrovski <dombrovskia@upmc.edu>
2021-09-07 16:14:24 -0400

    Updated AIC plots, V_max TF analysis prototype

--- meg/code/medusoid/aic_plots_dual_wholebrain.R
+++ meg/code/medusoid/aic_plots_dual_wholebrain.R
@@ -23,10 +23,10 @@ regressor = c("entropy_change")
 # regressors = c("entropy", "kld", "entropy_change", "entropy_change_neg", "entropy_change_pos", "reward")
 print_filenames = T
 fixed_only = F
-reprocess = T
+reprocess = F
 plots = T
+aic_threshold = 100
 diags = F
-average = F
 noclock = F
 setwd(data_dir)
 # plots ----
@@ -44,9 +44,11 @@ if (reprocess) {
     csl <- lapply(files, function(x) {
       if (print_filenames) { print(x) }
       df <- readRDS(x) 
-      if (ncol(df)==3) {
-        df <- df$fit_df
-      }
+      # if (class(df)=="list") {
+      df <- df$fit_df
+      # } else if (ncol(df)==3) {
+      # df <- df$fit_df
+      # }
       return(df)
     })
     csdf <- data.table::rbindlist(csl) %>% unique() %>% distinct(Time, Freq, .keep_all = TRUE)
@@ -61,9 +63,9 @@ if (reprocess) {
     cfl <- lapply(files, function(x) {
       if (print_filenames) { print(x) }
       df <- readRDS(x) 
-      if (ncol(df)==3) {
-        df <- df$fit_df
-      }
+      # if (ncol(df)==3) {
+      df <- df$fit_df
+      # }
       return(df)
     })
     cfdf <- data.table::rbindlist(cfl) %>% unique() %>% distinct(Time, Freq, .keep_all = TRUE)
@@ -80,9 +82,9 @@ if (reprocess) {
   rsl <- lapply(files, function(x) {
     if (print_filenames) { print(x) }
     df <- readRDS(x) 
-    if (ncol(df)==3) {
-      df <- df$fit_df
-    }
+    # if (ncol(df)==3) {
+    df <- df$fit_df
+    # }
     #        df <- df %>% filter(effect=="fixed")
     return(df)
   })
@@ -97,9 +99,9 @@ if (reprocess) {
   rfl <- lapply(files, function(x) {
     if (print_filenames) { print(x) }
     df <- readRDS(x) 
-    if (ncol(df)==3) {
-      df <- df$fit_df
-    }
+    # if (ncol(df)==3) {
+    df <- df$fit_df
+    # }
     #        df <- df %>% filter(effect=="fixed")
     return(df)
   })
@@ -115,25 +117,37 @@ if (reprocess) {
   ddf$Freq <- gsub("f_", "", ddf$Freq)
   ddf$Freq <- ordered(as.numeric(substr(as.character(ddf$Freq), 1,4)))    
   ddf <- ddf %>% arrange(Time, Freq, alignment, model) %>% mutate(row = row_number()) 
+  if (!noclock) {ddf <- rbind(cddf, rddf)} else {
+    ddf <- rddf  
+    wddf <- pivot_wider(ddf %>% select(Time, t, Freq, AIC, BIC, model, alignment), names_from = model, names_sep = "_", values_from = c(AIC, BIC)) %>%
+      mutate(AIC_diff = AIC_full - AIC_selective,
+             BIC_diff = BIC_full - BIC_selective)
+  }
   # convert to wide
   wddf <- pivot_wider(ddf %>% select(Time, t, Freq, AIC, BIC, model, alignment), names_from = model, names_sep = "_", values_from = c(AIC, BIC)) %>%
     mutate(AIC_diff = AIC_full - AIC_selective,
-           AIC_log_diff = log(AIC_diff),
-           BIC_diff = BIC_full - BIC_selective,
-           BIC_log_diff = log(BIC_diff))
-  saveRDS(wddf, file = paste0("meg_ddf_wholebrain_compression_", regressor, ".rds"))
-  
-  # saveRDS(rddf, file = "meg_ddf_wholebrain_ec_rs_rt.rds")
+           BIC_diff = BIC_full - BIC_selective)
+  saveRDS(ddf, file = paste0("meg_ddf_wholebrain_compression_", regressor, ".rds"))
+  saveRDS(wddf, file = paste0("meg_wddf_wholebrain_compression_", regressor, ".rds"))
+} else {
+  setwd(paste0(plot_dir, "/", "compression"))
+  wddf <- readRDS(paste0("meg_wddf_wholebrain_compression_", regressor, ".rds"))
+}
+
+if (plots) {
   message("Processed output, plotting.  \n")
-  
-  p1 <- ggplot(wddf %>% filter(alignment=="clock" & abs(AIC_diff) > 100), aes(Time, Freq)) + geom_tile(aes(fill = AIC_diff)) + 
-    scale_fill_viridis(option = "plasma") + theme_dark()
-  p2 <- ggplot(wddf %>% filter(alignment=="rt" & abs(AIC_diff) > 100), aes(Time, Freq)) + geom_tile(aes(fill = AIC_diff)) + 
-    scale_fill_viridis(option = "plasma") + theme_dark()
-  ggarrange(p2, p1)
+  # p1 <- ggplot(wddf %>% filter(alignment=="clock" & abs(AIC_diff) > aic_threshold), aes(Time, Freq)) + geom_tile(aes(fill = AIC_diff)) + 
+  #   scale_fill_viridis(option = "plasma") + theme_dark()
+  # p2 <- ggplot(wddf %>% filter(alignment=="rt" & abs(AIC_diff) > aic_threshold), aes(Time, Freq)) + geom_tile(aes(fill = AIC_diff)) + 
+  #   scale_fill_viridis(option = "plasma") + theme_dark()
+  # ggarrange(p2, p1)
   fname = paste0("meg_tf_compression_", regressor, ".pdf")
+  wddf <- wddf %>% mutate(AIC_diff_thresholded  = case_when(
+    abs(AIC_diff) > aic_threshold ~ AIC_diff,
+    TRUE ~ NA_real_
+  ))
   pdf(fname, width = 10, height = 5)
-  ggplot(wddf %>% filter(abs(AIC_diff) > 100), aes(t, Freq)) + geom_tile(aes(fill = AIC_diff)) + 
+  ggplot(wddf , aes(t, Freq)) + geom_tile(aes(fill = AIC_diff_thresholded)) + 
     geom_vline(xintercept = 0, lty = "dashed", color = "black", size = 2) +
     geom_vline(xintercept = -offset + 0.3, lty = "dashed", color = "white", size = 2) +
     geom_vline(xintercept = -offset, lty = "dashed", color = "white", size = 1) +
@@ -145,14 +159,4 @@ if (reprocess) {
     scale_x_continuous(limits = c(-5.3,1.7), breaks = c(0-offset+0.3, 0.20-offset+0.3, 0.4-offset+0.3, 0.60-offset+0.3, 1-offset+0.3, 1.5-offset+0.3, 0, 1), labels = c("0", "0.2", "0.4", "0.6", "1", "1.5",  "0", "1")) +
     labs(fill = expression("AIC"["full"] - AIC["selective"])) + ggtitle("Evidence of value compression") + theme_dark()
   dev.off()
-  if (!noclock) {ddf <- rbind(cddf, rddf)} else {
-    ddf <- rddf  
-    wddf <- pivot_wider(ddf %>% select(Time, t, Freq, AIC, BIC, model, alignment), names_from = model, names_sep = "_", values_from = c(AIC, BIC)) %>%
-      mutate(AIC_diff = AIC_full - AIC_selective,
-             AIC_log_diff = log(AIC_diff),
-             BIC_diff = BIC_full - BIC_selective,
-             BIC_log_diff = log(BIC_diff))
-    saveRDS(wddf, file = paste0("meg_ddf_wholebrain_compression_RT_", regressor, ".rds"))
-  }
-  setwd(paste0(plot_dir, "/", "compression"))
 }
