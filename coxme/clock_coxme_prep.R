0a19eef987f32369b32d9c67ba1c2f2804f2e073
Alex Dombrovski <dombrovskia@upmc.edu>
2022-04-26 16:18:42 -0400

    Refreshed script merging survival object with temporally aligned (within trial) decon

--- coxme/clock_coxme_prep.R
+++ coxme/clock_coxme_prep.R
@@ -16,11 +16,11 @@ library(coxme)
 library(survminer)
 # library(OIsurv)
 library(ggpubr)
-# devtools::install_github('junkka/ehahelper') # requires gfortran, $ brew cask install gfortran
-library(ehahelper)
-library(broom)
-library(broom.mixed)
-library(car)
+# # devtools::install_github('junkka/ehahelper') # requires gfortran, $ brew cask install gfortran
+# library(ehahelper)
+# library(broom)
+# library(broom.mixed)
+# library(car)
 # load(file="clock_for_coxme_value_only_070518.RData")
 # msdf <- read_csv("~/code/clock_analysis/coxme/sceptic_signals/mmclock_meg_fixed_uv_ureset_fixedparams_meg_ffx_trial_statistics.csv.gz")
 
@@ -29,6 +29,7 @@ library(car)
 ## Prep fMRI data
 #################
 
+# whether to add medusa data
 medusa = T
 
 load(file="clock_for_coxme_value_only_070518.RData")
@@ -37,13 +38,14 @@ load(file="clock_for_coxme_value_only_070518.RData")
 # sanity checks on within-trial matrices
 
 # add DAN and other betas, trial-level vars
-setwd(file.path(basedir, 'clock_analysis/fmri/keuka_brain_behavior_analyses/'))
-load('trial_df_and_vh_pe_clusters_u.Rdata')
-hdf <- df %>% select (ID, run, run_trial, DAN, dan_parietal, dan_l_sfg, dan_r_sfg, general_entropy,
-                      pe_f1_cort_hipp, pe_f2_cerebell, pe_f3_str, pe_PH_r, pe_ips, med_par, fef, entropy_vlPFC,
-                      omission_lag, omission_lag2, omission_lag3)
+# setwd(file.path(basedir, 'clock_analysis/fmri/keuka_brain_behavior_analyses/'))
+
+# load('trial_df_and_vh_pe_clusters_u.Rdata')
+# hdf <- df %>% select (ID, run, run_trial, DAN, dan_parietal, dan_l_sfg, dan_r_sfg, #general_entropy,
+                      # pe_f1_cort_hipp, pe_f2_cerebell, pe_f3_str, pe_PH_r, pe_ips, med_par, fef, entropy_vlPFC,
+                      # omission_lag, omission_lag2, omission_lag3)
 sdf$run_trial <- sdf$trial
-sdf <- sdf %>% inner_join(hdf, by = c("ID", "run", "run_trial"))
+# sdf <- sdf %>% inner_join(hdf, by = c("ID", "run", "run_trial"))
 
 # add evt_times for MEDUSA merge
 sdf$evt_time = floor(sdf$t2)
@@ -158,128 +160,130 @@ mvudf$response <- mvudf$y_chosen==mvudf$bin
 # test coxme
 # summary(test <- coxme(Surv(t1,t2,response) ~ v + u +
 #                                 (1|id), mdf))
-
-# add trial-level variables
-
-load("../fmri/keuka_brain_behavior_analyses/trial_df_and_vh_pe_clusters_u.Rdata")
-msdf <- merge(mdf, mvudf)
-# ggplot(msdf, aes(bin, v, color = rewFunc)) + geom_smooth()
-
-msdf$trial <- as.numeric(msdf$trial)
-msdf$timestep <- msdf$y_chosen
-# get within-subject and within-trial value and uncertainty
-msdf <- msdf %>% group_by(ID, run) %>% mutate(value_wi = scale(v),
-                                         uncertainty_wi = scale(u),
-                                         value_b = mean(v),
-                                         uncertainty_b = mean(u), 
-                                         trial_neg_inv_sc = scale(-1/run_trial)) %>% ungroup() %>%
-  group_by(ID, run, run_trial) %>% mutate(value_wi_t = scale(v),
-                                          uncertainty_wi_t = scale(u),
-                                          value_b_t = mean(v),
-                                          uncertainty_b_t = mean(u)) %>% ungroup()
-
-# filter out no-go zones at the edges
-# censoring the entire first second seems too harsh, remove first 500 ms
-mfdf <- msdf %>% filter(bin > 5 & bin <35)
-
-# diagnostics on uncertainty and value distributions
-setwd('~/OneDrive/collected_letters/papers/meg/plots/uv')
-pdf("meg_v_diagnostics.pdf", height = 6, width = 10)
-ggplot(msdf %>% filter(bin >5 & bin <35), aes(timestep, value_wi, color = rewFunc)) + geom_smooth(method = 'gam', formula = y~splines::ns(x,2)) 
-dev.off()
-pdf("meg_u_diagnostics.pdf", height = 6, width = 10)
-ggplot(msdf %>% filter(bin >5 & bin <35), aes(timestep, uncertainty_wi_t, color = rewFunc)) + geom_smooth(method = 'gam', formula = y~splines::ns(x,2)) 
-dev.off()
-
-
-# ggplot(msdf %>% filter(bin >10 & bin <35), aes(time, uncertainty_wi, color = rewFunc)) + geom_smooth(method = 'gam', formula = y~splines::ns(x,2))
-# corr.test(fdf$value_wi, fdf$uncertainty_wi)
-# corr.test(fdf$value, fdf$uncertainty)
 # 
-
-
-##Build 0/1 WV smiles (selection history with different buffer sizes)
-# msdf <- msdf %>% select(ID, run, trial, bin, everything())
-
-#use trial-wise data frame for getting lagged timesteps
-trialwise <- msdf %>% filter(bin==1) %>% 
-  group_by(ID, run) %>%
-  mutate(timesteplag1=dplyr::lag(timestep, n=1, order_by=run_trial),
-         timesteplag2=dplyr::lag(timestep, n=2, order_by=run_trial),
-         timesteplag3=dplyr::lag(timestep, n=3, order_by=run_trial),
-         timesteplag4=dplyr::lag(timestep, n=4, order_by=run_trial)) %>%
-  ungroup() %>% select(ID, run, trial, timesteplag1, timesteplag2, timesteplag3, timesteplag4)
-
-# msdf <- msdf %>% select(-timesteplag) %>% left_join(trialwise, by=c("ID", "run", "trial"))
-msdf <- msdf %>% left_join(trialwise, by=c("ID", "run", "trial"))
-
-
-get_wv_smile <- function(microdf, nlags=3, nbefore=0, nafter=1, spec=FALSE) {
-  smile <- rep(0, nrow(microdf))
-  lcols <- ifelse(isTRUE(spec), paste0("timesteplag", nlags), paste0("timesteplag", 1:nlags))
-  wvprefix <- ifelse(isTRUE(spec), "wvs", "wv")
-  vv <- microdf[1,lcols] #just first row (bin) of this trial is needed
-  if (nrow(vv) > 0L) {
-    allpos <- lapply(vv, function(x) {
-      if (is.na(x)) {
-        return(NULL)
-      } else {
-        return(seq.int(x-nbefore, x+nafter))
-      }
-    })
-    
-    topopulate <- unname(unlist(allpos))
-    smile[topopulate[topopulate <= nrow(microdf)]] <- 1
-  }
-  microdf[[paste0(wvprefix, nlags, "b", nbefore, "a", nafter)]] <- smile
-  return(microdf)
-}
-
-#get_wv_smile assumes bin-ordered data
-msdf <- msdf %>% arrange(ID, run, trial, bin)
-
-msdf$splitbasis <- with(msdf, paste(ID, run, trial, sep="."))
-
-splitdf <- split(msdf, msdf$splitbasis)
-splitdf <- lapply(splitdf, function(microdf) {
-  microdf %>% get_wv_smile(n=3, nbefore=0, nafter=1) %>%
-    get_wv_smile(n=3, nbefore=1, nafter=1) %>%
-    get_wv_smile(n=3, nbefore=1, nafter=2) %>%
-    get_wv_smile(n=4, nbefore=0, nafter=1) %>%
-    get_wv_smile(n=4, nbefore=1, nafter=1) %>%
-    get_wv_smile(n=4, nbefore=1, nafter=2) %>%
-    get_wv_smile(n=1, nbefore=1, nafter=1, spec=TRUE) %>%
-    get_wv_smile(n=2, nbefore=1, nafter=1, spec=TRUE) %>%
-    get_wv_smile(n=3, nbefore=1, nafter=1, spec=TRUE) %>%
-    get_wv_smile(n=4, nbefore=1, nafter=1, spec=TRUE)
-})
-
-mbb <- bind_rows(splitdf)
-
-mfbb <- mbb %>% filter(bin >10 & bin <35)
-
-#spot check
-# mbb %>% filter(trial > 5) %>% select(bin, timestep, timesteplag1, timesteplag2, wv3b0a1)
-#  table(mbb$wv3b0a1)
-#  table(mbb$wv4b0a1)
-
-## strategy for merging MEDUSA data: 1. write downsampled evt-time to coxme df, 2. merge in long MEDUSA df by id, run, trial, evt_time
-
-# spot-check obsessively
-# pdf('meg_check.pdf', height = 6, width = 6)
-# ggplot(mbb, aes(run_trial, value_wi, color = rewFunc)) + geom_smooth()
+# 
+# # add trial-level variables
+# load("../fmri/keuka_brain_behavior_analyses/trial_df_and_vh_pe_clusters_u.Rdata")
+# msdf <- merge(mdf, mvudf)
+# # ggplot(msdf, aes(bin, v, color = rewFunc)) + geom_smooth()
+# 
+# msdf$trial <- as.numeric(msdf$trial)
+# msdf$timestep <- msdf$y_chosen
+# # get within-subject and within-trial value and uncertainty
+# msdf <- msdf %>% group_by(ID, run) %>% mutate(value_wi = scale(v),
+#                                          uncertainty_wi = scale(u),
+#                                          value_b = mean(v),
+#                                          uncertainty_b = mean(u), 
+#                                          trial_neg_inv_sc = scale(-1/run_trial)) %>% ungroup() %>%
+#   group_by(ID, run, run_trial) %>% mutate(value_wi_t = scale(v),
+#                                           uncertainty_wi_t = scale(u),
+#                                           value_b_t = mean(v),
+#                                           uncertainty_b_t = mean(u)) %>% ungroup()
+# 
+# # filter out no-go zones at the edges
+# # censoring the entire first second seems too harsh, remove first 500 ms
+# mfdf <- msdf %>% filter(bin > 5 & bin <35)
+# 
+# # diagnostics on uncertainty and value distributions
+# setwd('~/OneDrive/collected_letters/papers/meg/plots/uv')
+# pdf("meg_v_diagnostics.pdf", height = 6, width = 10)
+# ggplot(msdf %>% filter(bin >5 & bin <35), aes(timestep, value_wi, color = rewFunc)) + geom_smooth(method = 'gam', formula = y~splines::ns(x,2)) 
+# dev.off()
+# pdf("meg_u_diagnostics.pdf", height = 6, width = 10)
+# ggplot(msdf %>% filter(bin >5 & bin <35), aes(timestep, uncertainty_wi_t, color = rewFunc)) + geom_smooth(method = 'gam', formula = y~splines::ns(x,2)) 
 # dev.off()
 # 
-# pdf('fmri_check.pdf', height = 6, width = 6)
-# p1 <- ggplot(bb, aes(run_trial, value_wi, color = rewFunc)) + geom_smooth(method = 'gam')
-# p2 <- ggplot(bb, aes(t1, value_wi, color = rewFunc)) + geom_smooth(method = 'gam')
-# p3 <- ggplot(bb, aes(run_trial, uncertainty_wi, color = rewFunc)) + geom_smooth(method = 'gam')
-# p4 <- ggplot(bb, aes(t1, uncertainty_wi, color = rewFunc)) + geom_smooth(method = 'gam')
-# ggarrange(p1,p2, p3, p4)
-# dev.off()
+# 
+# # ggplot(msdf %>% filter(bin >10 & bin <35), aes(time, uncertainty_wi, color = rewFunc)) + geom_smooth(method = 'gam', formula = y~splines::ns(x,2))
+# # corr.test(fdf$value_wi, fdf$uncertainty_wi)
+# # corr.test(fdf$value, fdf$uncertainty)
+# # 
+# 
+# 
+# ##Build 0/1 WV smiles (selection history with different buffer sizes)
+# # msdf <- msdf %>% select(ID, run, trial, bin, everything())
+# 
+# #use trial-wise data frame for getting lagged timesteps
+# trialwise <- msdf %>% filter(bin==1) %>% 
+#   group_by(ID, run) %>%
+#   mutate(timesteplag1=dplyr::lag(timestep, n=1, order_by=run_trial),
+#          timesteplag2=dplyr::lag(timestep, n=2, order_by=run_trial),
+#          timesteplag3=dplyr::lag(timestep, n=3, order_by=run_trial),
+#          timesteplag4=dplyr::lag(timestep, n=4, order_by=run_trial)) %>%
+#   ungroup() %>% select(ID, run, trial, timesteplag1, timesteplag2, timesteplag3, timesteplag4)
+# 
+# # msdf <- msdf %>% select(-timesteplag) %>% left_join(trialwise, by=c("ID", "run", "trial"))
+# msdf <- msdf %>% left_join(trialwise, by=c("ID", "run", "trial"))
+# 
+# 
+# get_wv_smile <- function(microdf, nlags=3, nbefore=0, nafter=1, spec=FALSE) {
+#   smile <- rep(0, nrow(microdf))
+#   lcols <- ifelse(isTRUE(spec), paste0("timesteplag", nlags), paste0("timesteplag", 1:nlags))
+#   wvprefix <- ifelse(isTRUE(spec), "wvs", "wv")
+#   vv <- microdf[1,lcols] #just first row (bin) of this trial is needed
+#   if (nrow(vv) > 0L) {
+#     allpos <- lapply(vv, function(x) {
+#       if (is.na(x)) {
+#         return(NULL)
+#       } else {
+#         return(seq.int(x-nbefore, x+nafter))
+#       }
+#     })
+#     
+#     topopulate <- unname(unlist(allpos))
+#     smile[topopulate[topopulate <= nrow(microdf)]] <- 1
+#   }
+#   microdf[[paste0(wvprefix, nlags, "b", nbefore, "a", nafter)]] <- smile
+#   return(microdf)
+# }
+# 
+# #get_wv_smile assumes bin-ordered data
+# msdf <- msdf %>% arrange(ID, run, trial, bin)
+# 
+# msdf$splitbasis <- with(msdf, paste(ID, run, trial, sep="."))
+# 
+# splitdf <- split(msdf, msdf$splitbasis)
+# splitdf <- lapply(splitdf, function(microdf) {
+#   microdf %>% get_wv_smile(n=3, nbefore=0, nafter=1) %>%
+#     get_wv_smile(n=3, nbefore=1, nafter=1) %>%
+#     get_wv_smile(n=3, nbefore=1, nafter=2) %>%
+#     get_wv_smile(n=4, nbefore=0, nafter=1) %>%
+#     get_wv_smile(n=4, nbefore=1, nafter=1) %>%
+#     get_wv_smile(n=4, nbefore=1, nafter=2) %>%
+#     get_wv_smile(n=1, nbefore=1, nafter=1, spec=TRUE) %>%
+#     get_wv_smile(n=2, nbefore=1, nafter=1, spec=TRUE) %>%
+#     get_wv_smile(n=3, nbefore=1, nafter=1, spec=TRUE) %>%
+#     get_wv_smile(n=4, nbefore=1, nafter=1, spec=TRUE)
+# })
+# 
+# mbb <- bind_rows(splitdf)
+# 
+# mfbb <- mbb %>% filter(bin >10 & bin <35)
+# 
+# #spot check
+# # mbb %>% filter(trial > 5) %>% select(bin, timestep, timesteplag1, timesteplag2, wv3b0a1)
+# #  table(mbb$wv3b0a1)
+# #  table(mbb$wv4b0a1)
+# 
+# ## strategy for merging MEDUSA data: 1. write downsampled evt-time to coxme df, 2. merge in long MEDUSA df by id, run, trial, evt_time
+# 
+# # spot-check obsessively
+# # pdf('meg_check.pdf', height = 6, width = 6)
+# # ggplot(mbb, aes(run_trial, value_wi, color = rewFunc)) + geom_smooth()
+# # dev.off()
+# # 
+# # pdf('fmri_check.pdf', height = 6, width = 6)
+# # p1 <- ggplot(bb, aes(run_trial, value_wi, color = rewFunc)) + geom_smooth(method = 'gam')
+# # p2 <- ggplot(bb, aes(t1, value_wi, color = rewFunc)) + geom_smooth(method = 'gam')
+# # p3 <- ggplot(bb, aes(run_trial, uncertainty_wi, color = rewFunc)) + geom_smooth(method = 'gam')
+# # p4 <- ggplot(bb, aes(t1, uncertainty_wi, color = rewFunc)) + geom_smooth(method = 'gam')
+# # ggarrange(p1,p2, p3, p4)
+# # dev.off()
+# 
+
 
 # merge in MEDUSA data -- I wonder if we have some missing censored evt_times
-
+# 
 # use approx to interpolate
 
 # save w/o MEDUSA
