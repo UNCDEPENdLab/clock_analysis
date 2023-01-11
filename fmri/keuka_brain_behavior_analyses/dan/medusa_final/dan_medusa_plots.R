28e4f241cf349dc3e8222d93df310b58097b9ffe
Michael Hallquist <michael.hallquist@gmail.com>
2023-01-11 17:16:24 -0500

    updated dan medusa plots

--- fmri/keuka_brain_behavior_analyses/dan/medusa_final/dan_medusa_plots.R
+++ fmri/keuka_brain_behavior_analyses/dan/medusa_final/dan_medusa_plots.R
@@ -31,6 +31,113 @@ ddf$coef_df_reml <- ddf$coef_df_reml %>% dplyr::filter(evt_time <= 5 & effect=="
 plot_medusa(ddf, x="evt_time", y="estimate", ymin="estimate - std.error", ymax="estimate + std.error", color="vm_gradient17", facet_by="side",
             out_dir=file.path(out_dir, "rt_encode_400_final47_6Jul2022"), p.value="p_FDR")
 
+# manual plot of ABSPE final
+get_df <- function(ddf, model_name, term) {
+  require(dplyr)
+  ddf$coef_df_reml %>% filter(model_name==!!model_name & effect=="fixed" & evt_time <= 5) %>%
+    filter(term == !!term) %>%
+    mutate(..p=p.adjust(p.value, method="fdr")) %>%
+    mutate(
+      p_level = case_when(
+        ..p > .05 ~ '1',
+        ..p < .05 & ..p > .01 ~ '2',
+        ..p < .01 & ..p > .001 ~ '3',
+        ..p <.001 ~ '4'),
+      p_level = ordered(p_level, levels = c('1', '2', '3', '4'), labels = c("NS","p < .05", "p < .01", "p < .001"))
+    ) %>%
+    mutate(
+      vm_gradient17 = ordered(vm_gradient17, levels=c("MT+", "PPCcaudal", "PPCrostral", "Premotor"), labels=c("MT+", "Caudal PPC", "Rostral PPC", "Premotor"))
+    ) %>%
+    filter(evt_time < 5) # only 0-4 post-feedback
+}
+
+ddf <- readRDS("/Volumes/GoogleDrive/My Drive/SCEPTIC_fMRI/dan_medusa/rt_400_final47_encode_medusa_fmri_rslopeonly.rds")
+
+
+# final color assignments
+colors <- RColorBrewer::brewer.pal(4, "Dark2") %>% setNames(c("MT+", "Premotor", "Rostral PPC", "Caudal PPC"))
+colors <- colors[c("MT+", "Caudal PPC", "Rostral PPC", "Premotor")] # order along visuomotor transformation
+
+# common elements
+common <- list(
+  geom_hline(yintercept = 0, size=1.5, alpha=0.6),
+  geom_vline(xintercept = 0, size=1.5, alpha=0.6),
+  geom_line(mapping = aes(size=NULL), size=1.0, alpha=0.6, position=position_dodge(width=0.8)),
+  geom_pointrange(position=position_dodge(width=0.8)),
+  scale_size_manual("FDR-corrected p", values=c(0.3, 0.7, 1.1, 1.6)),
+  theme_bw(base_size=15),
+  scale_color_manual("Visuomotor\ngradient", values=colors)
+)
+
+my_df <- get_df(ddf, "enc_rt_abspe_logkld_rslope", "abs_pe")
+
+
+pdf("/Users/hallquist/Library/CloudStorage/OneDrive-Personal/collected_letters/papers/meg/figures/fig_3_PE_whole_brain_medusa_bb/abs_pe_medusa.pdf", width=8, height=5)
+ggplot(my_df, aes(x=evt_time, y=estimate, ymin=estimate-std.error, ymax=estimate + std.error, color=vm_gradient17, group=vm_gradient17, size=p_level)) +
+  common +
+  ylab("abs(PE) coefficient (AU)") + xlab("Time relative to feedback (seconds)")
+dev.off()
+
+
+
+# rew > om
+my_df <- get_df(ddf, "enc_rt_abspe_logkld_rslope", "outcomeReward")
+
+pdf("/Users/hallquist/Library/CloudStorage/OneDrive-Personal/collected_letters/papers/meg/figures/fig_3_PE_whole_brain_medusa_bb/rewom_medusa.pdf", width=8, height=5)
+ggplot(my_df, aes(x=evt_time, y=estimate, ymin=estimate-std.error, ymax=estimate + std.error, color=vm_gradient17, group=vm_gradient17, size=p_level)) +
+  common +
+  ylab("Reward > omission coefficient (AU)") + xlab("Time relative to feedback (seconds)")
+dev.off()
+
+#rew > om simple plot (no pe in model)
+my_df <- get_df(ddf, "enc_rt_rewom_rslope", "outcomeReward")
+
+pdf("/Users/hallquist/Library/CloudStorage/OneDrive-Personal/collected_letters/papers/meg/figures/fig_3_PE_whole_brain_medusa_bb/rewom_nope_medusa.pdf", width=8, height=5)
+ggplot(my_df, aes(x=evt_time, y=estimate, ymin=estimate-std.error, ymax=estimate + std.error, color=vm_gradient17, group=vm_gradient17, size=p_level)) +
+  common +
+  ylab("Reward > omission coefficient (AU)") + xlab("Time relative to feedback (seconds)")
+dev.off()
+
+
+# signed pe
+my_df <- get_df(ddf, "enc_rt_pe_logkld_rslope", "pe_max")
+
+pdf("/Users/hallquist/Library/CloudStorage/OneDrive-Personal/collected_letters/papers/meg/figures/fig_3_PE_whole_brain_medusa_bb/pe_medusa.pdf", width=8, height=5)
+ggplot(my_df, aes(x=evt_time, y=estimate, ymin=estimate-std.error, ymax=estimate + std.error, color=vm_gradient17, group=vm_gradient17, size=p_level)) +
+  common +
+  ylab("Prediction error coefficient (AU)") + xlab("Time relative to feedback (seconds)")
+dev.off()
+
+
+# vmax in abspe model
+my_df <- get_df(ddf, "enc_rt_abspe_logkld_rslope", "v_max_wi")
+
+
+pdf("/Users/hallquist/Library/CloudStorage/OneDrive-Personal/collected_letters/papers/meg/figures/fig_5_echange_whole_brain_medusa_bb_coxme/vmax_medusa.pdf", width=8, height=5)
+ggplot(my_df, aes(x=evt_time, y=estimate, ymin=estimate-std.error, ymax=estimate + std.error, color=vm_gradient17, group=vm_gradient17, size=p_level)) +
+  common +
+  ylab("Vmax coefficient (AU)") + xlab("Time relative to feedback (seconds)")
+dev.off()
+
+
+my_df <- get_df(ddf, "enc_rt_abspe_logkld_rslope", "v_entropy_wi_change")
+
+
+pdf("/Users/hallquist/Library/CloudStorage/OneDrive-Personal/collected_letters/papers/meg/figures/fig_5_echange_whole_brain_medusa_bb_coxme/v_entropy_wi_change_medusa.pdf", width=8, height=5)
+ggplot(my_df, aes(x=evt_time, y=estimate, ymin=estimate-std.error, ymax=estimate + std.error, color=vm_gradient17, group=vm_gradient17, size=p_level)) +
+  common +
+  ylab("Entropy change coefficient (AU)") + xlab("Time relative to feedback (seconds)")
+dev.off()
+
+
+
+
+
+
+
+
+
+
 #ddf <- readRDS("/Volumes/GoogleDrive/My Drive/SCEPTIC_fMRI/dan_medusa/clock_encode_medusa_fmri_scaled.rds")
 #ddf <- readRDS("/Volumes/GoogleDrive/My Drive/SCEPTIC_fMRI/dan_medusa/clock_encode_medusa_fmri.rds")
 ddf <- readRDS("/Volumes/GoogleDrive/My Drive/SCEPTIC_fMRI/dan_medusa/clock_400_final47_encode_medusa_fmri.rds")
